9b1cb1be8ebf23e2c455cdca4a6e76b6cb3dbc4b
JuliaKondria <galinakondria@yandex.ru>
2016-12-21 13:51:59 +0200

    add doc to UserDataListener class

--- edu.english/src/edu/english/data/UserDataListener.java
+++ edu.english/src/edu/english/data/UserDataListener.java
@@ -1,9 +1,17 @@
 package edu.english.data;
 
+/**
+ * Интерфейс слушателя Юзера
+ * 
+ * Надо реализовать тот интерфейс классом который хочет получать сообщения о измененияз в ДАТЕ юзера
+ */
 public interface UserDataListener {
 
 	public void notify(Type type, Object data);
 
+	/**
+	 * Типы сообщений от юзера
+	 */
 	public static enum Type {
 		KNOWN_WORDS_CHANGED,
 		UNKNOWN_WORDS_CHANGED,
