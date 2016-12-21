6e797bfa567af669934c8d7102549b8189dca8c5
tanyafurman <fff001998@gmail.com>
2016-12-21 12:49:31 +0200

    update icons

--- edu.english/src/edu/english/ui/IconProvier.java
+++ edu.english/src/edu/english/ui/IconProvier.java
@@ -4,13 +4,13 @@ import javax.swing.ImageIcon;
 
 public class IconProvier {
 
-	public static final ImageIcon TRUE_ICON = getIcon("true.gif");
+	public static final ImageIcon TRUE_ICON = getIcon("plus.png");
 	
-	public static final ImageIcon FALSE_ICON = getIcon("false.gif");
+	public static final ImageIcon FALSE_ICON = getIcon("cross.png");
 
-	public static final ImageIcon DEFAULT_ICON = getIcon("default.gif");
+	public static final ImageIcon DEFAULT_ICON = getIcon("minus.png");
 
 	private static ImageIcon getIcon(String name) {
-		return new ImageIcon(IconProvier.class.getResource(name), name);
+		return new ImageIcon(new ImageIcon(IconProvier.class.getResource(name), name).getImage().getScaledInstance(32, 32, java.awt.Image.SCALE_DEFAULT));
 	}
 }
